1612a119c79f790ef05fe1e3ad990f7d9f2aa203
이호민 <leehomin118@gmail.com>
2019-05-23 00:33:55 +0900

    add members cpp file

---
+++ CoalaMC_Teamwork/JGH.cpp
@@ -0,0 +1,6 @@
+#include <CoalaMOD.h>
+#include "block_constants.h"
+#include "utils.h"
+#pragma comment(lib, "CoalaMOD.lib")
+//건물 제작 함수를 만들어주시면 됩니다.
+//main함수를 쓸 필요는 없습니다.
--- CoalaMC_Teamwork/buildings.h
+++ CoalaMC_Teamwork/buildings.h
@@ -1,3 +1,5 @@
 #pragma once
 #include "LHM.h"
 #include "SYJ.h"
+#include "KJH.h"
+#include "JGH.h"
