fac551a25152a6c99cf87c6790b53d8dd273bd99
이호민 <leehomin118@gmail.com>
2019-05-25 18:56:11 +0900

    add mossy cobblestone in road by create_random_number function

--- CoalaMC_Teamwork/road.cpp
+++ CoalaMC_Teamwork/road.cpp
@@ -7,7 +7,7 @@
 void roadx(int x, int z, int len) {
 	for (int i = z; i < z + 7; i++) {
 		for (int j = x; j < x + len; j++) {
-			locateBlock(block[12], j, 4, i);
+			locateBlock(block[12 + create_random_number(0, 1) * 13], j, 4, i);
 		}
 	}
 }
@@ -15,15 +15,16 @@ void roadx(int x, int z, int len) {
 void roadz(int x, int z, int len) {
 	for (int i = x; i < x + 7; i++) {
 		for (int j = z; j < z + len; j++) {
-			locateBlock(block[12], i, 4, j);
+			locateBlock(block[12 + create_random_number(0, 1) * 13], i, 4, j);
 		}
 	}
 }
 
 void create_road(int x, int z, int cntx, int cntz) {
-	//여기에서 x, z입력은 좌표가 아니라 x, z축의 블럭 개수를 의미한다.
+	//여기에서 x, z입력은 시작 좌표이고 각각 +방향으로 늘어난다.
+	//cntx, cntz는 각각 x, z방향의 블록 개수이다.
 	//각 블럭의 크기는 60 * 60의 크기이다.
-	//길의 폭읜 7칸이다.
+	//길의 폭은 7칸이다.
 	int tmpx = x, tmpz = z;
 
 	for (int i = 0; i < cntx + 1; i++) {
