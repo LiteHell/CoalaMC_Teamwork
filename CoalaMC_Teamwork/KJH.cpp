48931e72cc277d0ed8eb6ac40c8f1fa9db8203ec
장기훈 <semir0228@gmail.com>
2019-05-26 20:13:06 +0900

    장기훈: 건물 바닥 만듦

--- CoalaMC_Teamwork/KJH.cpp
+++ CoalaMC_Teamwork/KJH.cpp
@@ -5,20 +5,34 @@
 //건물 제작 함수를 만들어주시면 됩니다.
 //main함수를 쓸 필요는 없습니다.
 
-void createBuilding_KJH(int sx, int sy, int sz)
+void createBuilding_KJH(int sx, int sy, int sz) // sy는 4로 해 주세요
 {
 	BlockID glass = createBlock(BLOCK_GLASS);
+	BlockID iron = createBlock(BLOCK_IRON);
 	DoorID door = createDoor(DOOR_OAK, true, HINGE_LEFT);
 	
-	const int size = 21;
+	WoolID wool[4] = {
+		createWool(COLOR_WHITE),
+		createWool(COLOR_YELLOW),
+		createWool(COLOR_GREEN),
+		createWool(COLOR_ORANGE)
+	};
+/* 테스트용
+	int sx, sy, sz;
 	
+	sx = 300;
+	sy = 4;
+	sz = 300;
+*/
+	const int size = 21;
 	int ex, ez;
 	int ey_glass;
 
 	ex = sx + size - 1;
 	ez = sz + size - 1;
 	ey_glass = sy + 3;
-
+	
+	// 벽 생성
 	for (int y = sy; y <= ey_glass-1; y++)
 	{
 		for (int x = sx; x <= ex; x++)
@@ -39,6 +53,14 @@ void createBuilding_KJH(int sx, int sy, int sz)
 		}
 	}
 
+	//바닥, 천장 생성
+	for (int x = sx; x <= ex; x++)
+	{
+		for (int z = sz; z <= ez; z++)
+		{
+			locateBlock(iron, x, sy-1, z);
+		}
+	}
 	for (int x = sx; x <= ex; x++)
 	{
 		for (int z = sz; z <= ez; z++)
@@ -47,10 +69,14 @@ void createBuilding_KJH(int sx, int sy, int sz)
 		}
 	}
 
+	//문 생성
 	installDoor(door, (sx + ex) / 2, sy, sz, FACING_EAST);
 	installDoor(door, (sx + ex) / 2, sy, ez, FACING_WEST);
-	installDoor(door, sx, sy, (sz+ez)/2, FACING_NORTH);
-	installDoor(door, ex, sy, (sz+ez)/2, FACING_NORTH);
+	installDoor(door, sx, sy, (sz + ez) / 2, FACING_NORTH);
+	installDoor(door, ex, sy, (sz + ez) / 2, FACING_NORTH);
+
+	create_random_number(1, 4);
+
 
 	return;
 }
