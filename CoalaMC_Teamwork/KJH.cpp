1a7d9c5d89b11600fd063d397fbcac0ac03b1d13
장기훈 <semir0228@gmail.com>
2019-05-26 21:13:35 +0900

    장기훈: 건물 정의 계속

--- CoalaMC_Teamwork/KJH.cpp
+++ CoalaMC_Teamwork/KJH.cpp
@@ -7,33 +7,38 @@
 
 void createBuilding_KJH(int sx, int sy, int sz) // sy는 4로 해 주세요
 {
-	BlockID glass = createBlock(BLOCK_GLASS);
+		BlockID glass = createBlock(BLOCK_GLASS);
 	BlockID iron = createBlock(BLOCK_IRON);
 	DoorID door = createDoor(DOOR_OAK, true, HINGE_LEFT);
-	
+
 	WoolID wool[4] = {
 		createWool(COLOR_WHITE),
 		createWool(COLOR_YELLOW),
 		createWool(COLOR_GREEN),
 		createWool(COLOR_ORANGE)
 	};
-/* 테스트용
-	int sx, sy, sz;
 	
+	/*테스트용
+	int sx, sy, sz;
+
 	sx = 300;
 	sy = 4;
 	sz = 300;
-*/
+	*/
+	
 	const int size = 21;
 	int ex, ez;
 	int ey_glass;
 
+	int building;
+	int wool_color;
+
 	ex = sx + size - 1;
 	ez = sz + size - 1;
 	ey_glass = sy + 3;
-	
+
 	// 벽 생성
-	for (int y = sy; y <= ey_glass-1; y++)
+	for (int y = sy; y <= ey_glass - 1; y++)
 	{
 		for (int x = sx; x <= ex; x++)
 		{
@@ -58,14 +63,14 @@ void createBuilding_KJH(int sx, int sy, int sz) // sy는 4로 해 주세요
 	{
 		for (int z = sz; z <= ez; z++)
 		{
-			locateBlock(iron, x, sy-1, z);
+			locateBlock(iron, x, sy - 1, z);
 		}
 	}
 	for (int x = sx; x <= ex; x++)
 	{
 		for (int z = sz; z <= ez; z++)
 		{
-			locateBlock(glass, x, ey_glass ,z);
+			locateBlock(glass, x, ey_glass, z);
 		}
 	}
 
@@ -75,8 +80,75 @@ void createBuilding_KJH(int sx, int sy, int sz) // sy는 4로 해 주세요
 	installDoor(door, sx, sy, (sz + ez) / 2, FACING_NORTH);
 	installDoor(door, ex, sy, (sz + ez) / 2, FACING_NORTH);
 
-	create_random_number(1, 4);
+	
+	int sx_b, sy_b, sz_b;
+	int ex_b, ey_b, ez_b;
 
+	//첫번? 도형 sx+1, ey_glass+1, sz+1 ~ (sx+ex)/2 - 1, ey_glass + size/2 - 1, (sz+ez)/2 -1
+	//첫번째의 맞은편 도형 
+	for (int i = 0; i < 8; i++)
+	{
+		wool_color = create_random_number(0, 3);
+		building = create_random_number(1, 4);
+		switch (i)
+		{
+		case 0:
+			sx_b = sx + 1, sy_b = ey_glass + 1, sz_b = sz + 1;
+			ex_b = (sx + ex) / 2 - 1, ey_b = ey_glass + size / 2 - 1, (sz + ez) / 2 - 1;
+			break;
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		case 6:
+			break;
+		case 7:
+			break;
+		default:
+			continue;
+		}
 
+		switch (building)
+		{
+		case 1:
+			for (int x = sx_b; x <= ex_b; x++)
+			{
+				for (int y = sy_b; y <= ey_b; y++)
+				{
+					for (int z = sz_b; z <= ez_b; z++)
+					{
+						locateWool(wool[wool_color], x, y, z);
+					}
+				}
+			}
+			break;
+		case 2:
+			for (int x = sx_b; x <= ex_b; x++)
+			{
+				for (int y = sy_b; y <= ey_b; y++)
+				{
+					for (int z = sz_b; z <= ez_b; z++)
+					{/*
+						if (x)
+						{
+						locateWool(wool[wool_color], x, y, z);
+					*/}
+				}
+			}
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		default:
+			continue;
+		}
+	}
 	return;
 }
