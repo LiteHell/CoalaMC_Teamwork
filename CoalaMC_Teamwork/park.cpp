3b373d712b79f5d958f3c7753e26d8f46b578e6a
LeeHomin0118 <leehomin118@gmail.com>
2019-05-27 19:52:05 +0900

    add lake

--- CoalaMC_Teamwork/park.cpp
+++ CoalaMC_Teamwork/park.cpp
@@ -23,12 +23,22 @@ void park_tree_4(int x, int y, int z) {
 void park_lake(int x, int y, int z) {
 	//using polar coordinate
 	const double pi = 3.14159265358979;
+	const double radian = pi / 180;
 	int qx, qz;
 	for (int q = 0; q < 360; q++) {
-		int tmp_rand = create_random_number(10, 15);
-		for (int i = x; i < x + cos(q*(pi/180)) * tmp_rand; i++) {
-			for (int j = z; j < z + sin(q*(pi/180)) * tmp_rand; j++) {
-				locateWater(createWater(), i, y, j);
+		int tmp_random = create_random_number(10, 15);
+		if (cos(q * radian) > sin(q * radian)) {
+			int tmpz = z;
+			for (int i = x; i < x + cos(q * radian) * tmp_random; i += cos(q * radian) / sin(q * radian)) {
+				locateWater(createWater(), i, y, tmpz);
+				++tmpz;
+			}
+		}
+		else if (cos(q * radian) > sin(q * radian)) {
+			int tmpx = x;
+			for (int i = z; i < z + sin(q * radian) * tmp_random; i += sin(q * radian) / cos(q * radian)) {
+				locateWater(createWater(), tmpx, y, i);
+				++tmpx;
 			}
 		}
 	}
