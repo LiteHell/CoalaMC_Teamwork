560dd28f5d06854877214c93780d68fe16a920ba
장기훈 <semir0228@gmail.com>
2019-05-27 09:17:35 +0900

    뭘넣었다는거?

--- CoalaMC_Teamwork/KJH.cpp
+++ CoalaMC_Teamwork/KJH.cpp
@@ -17,18 +17,21 @@ void createBuilding_KJH(int sx, int sy, int sz) // sy는 4로 해 주세요
 		createWool(COLOR_GREEN),
 		createWool(COLOR_ORANGE)
 	};
-/* 테스트용 변수 설정
+/*테스트용 변수 설정
 	int sx, sy, sz;
 
-	sx = 300;
+	sx = 600;
 	sy = 4;
-	sz = 300;
+	sz = 800;
 */
 	const int size = 21;
 	
 	int ex, ez;
 	int ey_glass;
 	int mx, my, mz;
+
+	int sx_b, sy_b, sz_b;
+	int ex_b, ey_b, ez_b;
 
 	int building;
 	int wool_color;
@@ -82,13 +85,8 @@ void createBuilding_KJH(int sx, int sy, int sz) // sy는 4로 해 주세요
 	installDoor(door, (sx + ex) / 2, sy, ez, FACING_WEST);
 	installDoor(door, sx, sy, (sz + ez) / 2, FACING_NORTH);
 	installDoor(door, ex, sy, (sz + ez) / 2, FACING_NORTH);
-
 	
-	int sx_b, sy_b, sz_b;
-	int ex_b, ey_b, ez_b;
-	
-	//첫번? 도형 sx+1, ey_glass+1, sz+1 ~ (sx+ex)/2 - 1, ey_glass + size/2 - 1, (sz+ez)/2 -1
-	//첫번째의 맞은편 도형 
+	//도형들 생성
 	for (int i = 0; i < 8; i++)
 	{
 		wool_color = create_random_number(0, 3);
@@ -110,15 +108,23 @@ void createBuilding_KJH(int sx, int sy, int sz) // sy는 4로 해 주세요
 			break;
 		case 3:
 			sx_b = mx + 1, sy_b = ey_glass + 1, sz_b = mz + 1;
-			ex_b = ex - 1, sy_b = ey_glass + size / 2 - 1, ez_b = ez - 1;
+			ex_b = ex - 1, ey_b = ey_glass + size / 2 - 1, ez_b = ez - 1;
 			break;
 		case 4:
+			sx_b = sx + 1, sy_b = ey_glass + size/2 + 1, sz_b = sz + 1;
+			ex_b = mx - 1, ey_b = ey_glass + size - 1, ez_b = mz - 1;
 			break;
 		case 5:
+			sx_b = mx + 1, sy_b = ey_glass + size/2 + 1, sz_b = sz + 1;
+			ex_b = ex - 1, ey_b = ey_glass + size - 1, ez_b = mz - 1;
 			break;
 		case 6:
+			sx_b = sx + 1, sy_b = ey_glass + size/2 + 1, sz_b = mz + 1;
+			ex_b = mx - 1, ey_b = ey_glass + size - 1, ez_b = ez - 1;
 			break;
 		case 7:
+			sx_b = mx + 1, sy_b = ey_glass + size/2 + 1, sz_b = mz + 1;
+			ex_b = ex - 1, ey_b = ey_glass + size - 1, ez_b = ez - 1;
 			break;
 		default:
 			continue;
@@ -144,17 +150,35 @@ void createBuilding_KJH(int sx, int sy, int sz) // sy는 4로 해 주세요
 				for (int y = sy_b; y <= ey_b; y++)
 				{
 					for (int z = sz_b; z <= ez_b; z++)
-					{/*
-						if (x)
-						{
+					{
 						locateWool(wool[wool_color], x, y, z);
-					*/}
+					}
 				}
 			}
 			break;
 		case 3:
+			for (int x = sx_b; x <= ex_b; x++)
+			{
+				for (int y = sy_b; y <= ey_b; y++)
+				{
+					for (int z = sz_b; z <= ez_b; z++)
+					{
+						locateWool(wool[wool_color], x, y, z);
+					}
+				}
+			}
 			break;
 		case 4:
+			for (int x = sx_b; x <= ex_b; x++)
+			{
+				for (int y = sy_b; y <= ey_b; y++)
+				{
+					for (int z = sz_b; z <= ez_b; z++)
+					{
+						locateWool(wool[wool_color], x, y, z);
+					}
+				}
+			}
 			break;
 		default:
 			continue;
