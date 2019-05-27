d3a8c13117ef5a3476c7b816978f23f05fc23a30
GILKIDU <48651274+GILKIDU@users.noreply.github.com>
2019-05-27 12:38:21 +0900

    1차 완료(10분후 함수로 바꿉니당)

--- CoalaMC_Teamwork/KJH.cpp
+++ CoalaMC_Teamwork/KJH.cpp
@@ -4,7 +4,7 @@
 #include<CoalaMOD.h>
 #pragma comment(lib, "CoalaMOD.lib")
 
-int main()
+void createBuilding_MARINA()
 {
 	int sx = 0; int sy = 0; int sz = 0; int x, y, z; int a, b, c;
 	BlockID STONE = createBlock(BLOCK_STONE);
@@ -13,9 +13,12 @@ int main()
 	BlockID WOOL = createWool(COLOR_LIGHT_BLUE);
 	BlockID WHITE = createWool(COLOR_WHITE);
 	BlockID FLOOR = createBlock(BLOCK_DIORITE);
+	BlockID CEILING = createBlock(BLOCK_BEDROCK);
+	BlockID DOOR = createDoor(DOOR_IRON);
+	BlockID EMPTY = createBlock(BLOCK_AIR);
 	scanf("%d %d %d", &x, &y, &z);
 	a = 0; b = 0; c = 0;
-	for (sy = 29; sy < 31; sy++) {//두 빌딩을 연결하는 배 형태를 만드는 코드
+	for (sy = 45; sy < 47; sy++) {//두 빌딩을 연결하는 배 형태를 만드는 코드
 		for (; sx < 15; sx++) { locateBlock(WHITE, x + sx, sy, z + sz); }
 		for (; sx < 18; sx++) { locateBlock(WHITE, x + sx, sy, z + sz + 1); }
 		for (; sx < 20; sx++) { locateBlock(WHITE, x + sx, sy, z + sz + 2); }
@@ -23,8 +26,8 @@ int main()
 		for (; sx < 24; sx++) { locateBlock(WHITE, x + sx, sy, z + sz + 4); }
 		for (; sx < 26; sx++) { locateBlock(WHITE, x + sx, sy, z + sz + 5); }
 		for (; sx < 28; sx++) { locateBlock(WHITE, x + sx, sy, z + sz + 6); }
-		locateBlock(WHITE, x + sx, 29, z + sz + 7);
-		locateBlock(WHITE, x + sx, 30, z + sz + 7);
+		locateBlock(WHITE, x + sx, 45, z + sz + 7);
+		locateBlock(WHITE, x + sx, 46, z + sz + 7);
 		sx = 0; sz = 0;
 		for (; sx < 15; sx++) { locateBlock(WHITE, x + sx, sy, z + sz + 14); }
 		for (; sx < 18; sx++) { locateBlock(WHITE, x + sx, sy, z + sz + 13); }
@@ -38,7 +41,7 @@ int main()
 		sx = 0; sz = 0;
 	}
 	sy = 0;
-	for (; sy < 25; sy++) {// 기본 빌딩 틀을 만드는 코드
+	for (; sy < 41; sy++) {// 기본 빌딩 틀을 만드는 코드
 		sx = 4; sz = 0;
 		for (; sx < 13; sx++) {
 			locateBlock(STONE, x + sx, 4 + sy, z + sz);
@@ -52,26 +55,27 @@ int main()
 	}
 
 	sy = 0; sx = 0;
-	for (; sy < 22; sy += 5) {
+	for (; sy < 39; sy += 5) {
 		for (int a = 4; a < 7; a++)
 		{
 			for (sx = 6, sz = 0; sx < 11; sx++) {
-				locateBlock(WOOL, x + sx, sy + a, z + sz);
-				locateBlock(WOOL, x + sx, sy + a, z + sz + 14);
+				locateBlock(WOOL, x + sx, sy + a + 1, z + sz);
+				locateBlock(WOOL, x + sx, sy + a + 1, z + sz + 14);
 			}
 			sx = 4;
 			for (sz = 4; sz < 11; sz++)
 			{
-				locateBlock(WOOL, x + sx, sy + a, z + sz);
-				locateBlock(WOOL, x + sx + 8, sy + a, z + sz);
+				locateBlock(WOOL, x + sx, sy + a + 1, z + sz);
+				locateBlock(WOOL, x + sx + 8, sy + a + 1, z + sz);
 			}
 		}
 	}
 	//빌딩의 유리를 만드는 코드
 
 	b = 13;
-
-	for (; sy < 25; sy++) {// 기본 빌딩 틀을 만드는 코드
+	sx = 17;
+	sy = 0;
+	for (; sy < 41; sy++) {// 기본 빌딩 틀을 만드는 코드
 		sz = 0;
 		for (sx = 17; sx < 26; sx++) {
 			locateBlock(STONE, x + sx, 4 + sy, z + sz);
@@ -85,43 +89,111 @@ int main()
 	}
 
 	sy = 0; sx = b;
-	for (; sy < 22; sy += 5) {
+	for (; sy < 39; sy += 5) {
 		for (int a = 4; a < 7; a++)
 		{
 			for (sx = 6 + b, sz = 0; sx < 11 + b; sx++) {
-				locateBlock(WOOL, x + sx, sy + a, z + sz);
-				locateBlock(WOOL, x + sx, sy + a, z + sz + 14);
+				locateBlock(WOOL, x + sx, sy + a + 1, z + sz);
+				locateBlock(WOOL, x + sx, sy + a + 1, z + sz + 14);
 			}
 			sx = 4 + b;
 			for (sz = 4; sz < 11; sz++)
 			{
-				locateBlock(WOOL, x + sx, sy + a, z + sz);
-				locateBlock(WOOL, x + sx + 8, sy + a, z + sz);
+				locateBlock(WOOL, x + sx, sy + a + 1, z + sz);
+				locateBlock(WOOL, x + sx + 8, sy + a + 1, z + sz);
 			}
 		}
 	}
 	sx = 0; sz = 0;
 	for (int i = 1; i < 28; i++) {
-		locateBlock(FLOOR, x + sx + i, 29, z + sz + 7);
+		locateBlock(FLOOR, x + sx + i, 45, z + sz + 7);
 	}
-	for (int i = 1; i < 28; i++) {
-		locateBlock(FLOOR, x + sx + i, 29, z + sz + 7);
+	for (int i = 1; i < 26; i++) {
+		locateBlock(FLOOR, x + sx + i, 45, z + sz + 8);
+		locateBlock(FLOOR, x + sx + i, 45, z + sz + 6);
 	}
-	for (int i = 1; i < 28; i++) {
-		locateBlock(FLOOR, x + sx + i, 29, z + sz + 7);
+	for (int i = 1; i < 24; i++) {
+		locateBlock(FLOOR, x + sx + i, 45, z + sz + 9);
+		locateBlock(FLOOR, x + sx + i, 45, z + sz + 5);
 	}
-	for (int i = 1; i < 28; i++) {
-		locateBlock(FLOOR, x + sx + i, 29, z + sz + 7);
+	for (int i = 1; i < 22; i++) {
+		locateBlock(FLOOR, x + sx + i, 45, z + sz + 10);
+		locateBlock(FLOOR, x + sx + i, 45, z + sz + 4);
 	}
-	for (int i = 1; i < 28; i++) {
-		locateBlock(FLOOR, x + sx + i, 29, z + sz + 7);
+	for (int i = 1; i < 20; i++) {
+		locateBlock(FLOOR, x + sx + i, 45, z + sz + 11);
+		locateBlock(FLOOR, x + sx + i, 45, z + sz + 3);
 	}
-	for (int i = 1; i < 28; i++) {
-		locateBlock(FLOOR, x + sx + i, 29, z + sz + 7);
+	for (int i = 1; i < 18; i++) {
+		locateBlock(FLOOR, x + sx + i, 45, z + sz + 12);
+		locateBlock(FLOOR, x + sx + i, 45, z + sz + 2);
 	}
-	for (int i = 1; i < 28; i++) {
-		locateBlock(FLOOR, x + sx + i, 29, z + sz + 7);
+	for (int i = 1; i < 15; i++) {
+		locateBlock(FLOOR, x + sx + i, 45, z + sz + 13);
+		locateBlock(FLOOR, x + sx + i, 45, z + sz + 1);
 	}
-	//빌딩의 유리를 만드는 코드
-	return 0;
-}
+	for (sx = 4; sx < 13; sx++) {//천장과 바닥을 만드는 코드
+		for (sz = 0; sz < 15; sz++) {
+			locateBlock(CEILING, x + sx, 4, z + sz);
+			locateBlock(CEILING, x + sx, 44, z + sz);
+			locateBlock(CEILING, x + sx + 13, 4, z + sz);
+			locateBlock(CEILING, x + sx + 13, 44, z + sz);
+		}
+	}
+
+	sy = 46;
+	for (sx = 17; sx > 11; sx--) {
+		for (sz = 11; sz > 3; sz--)
+		{
+			locateBlock(WATER, x + sx, sy, z + sz);
+		}
+	}
+	for (sx = 20; sx > 17; sx--) {
+		for (sz = 5; sz < 10; sz++)
+		{
+			locateBlock(WATER, sx + x, sy, sz + z);
+		}
+	}
+
+	
+	sy = 46;
+		for (sx = 20, sz = 5; sz < 10; sz++) {
+			locateBlock(GOLD, x + sx, sy, z + sz);
+		}
+		sz = 0;
+		locateBlock(GOLD, x + 18, sy, z + sz + 5);
+		locateBlock(GOLD, x + 19, sy, z + sz + 5);
+		locateBlock(GOLD, x + 18, sy, z + sz + 9);
+		locateBlock(GOLD, x + 19, sy, z + sz + 9);
+		locateBlock(GOLD, x + 17, sy, z + sz + 4);
+		locateBlock(GOLD, x + 17, sy, z + sz + 10);
+		for (sx = 17; sx > 11; sx--)
+		{
+			locateBlock(GOLD, x + sx, sy, z + sz + 11);
+			locateBlock(GOLD, x + sx, sy, z + sz + 3);
+		}
+		for (sz = 11, sx++; sz > 3; sz--)
+		{
+			locateBlock(GOLD, x + sx, sy, z + sz);
+		}
+	
+		for (sy = 46; sy < 49; sy++)
+		{
+			sz = 0;
+			for (sx = 8; sx > 2; sx--)
+			{
+				locateBlock(CEILING, x + sx, sy, z + sz + 11);
+				locateBlock(CEILING, x + sx, sy, z + sz + 3);
+			}
+
+			for (sz = 11; sz > 2; sz--)
+			{
+				sx = 8;
+				locateBlock(CEILING, x + sx, sy, z + sz);
+				sx = 2;
+				locateBlock(CEILING, x + sx, sy, z + sz);
+			}
+		}
+		locateBlock(EMPTY, x + 8, 47, z + 8);
+		locateBlock(EMPTY, x + 8, 46, z + 8);
+	}
