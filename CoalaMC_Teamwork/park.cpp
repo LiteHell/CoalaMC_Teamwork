843e09dff2fefce3e5c3fe10d50ff26c8b2d8d7f
LeeHomin0118 <leehomin118@gmail.com>
2019-05-27 15:58:49 +0900

    create lake

--- CoalaMC_Teamwork/park.cpp
+++ CoalaMC_Teamwork/park.cpp
@@ -21,7 +21,18 @@ void park_tree_4(int x, int y, int z) {
 }
 
 void park_lake(int x, int y, int z) {
-
+	//using polar coordinate
+	const double pi = 3.14159265358979;
+	int qx, qz;
+	for (int q = 0; q < 360; q++) {
+		int tmp_rand = create_random_number(10, 15);
+		for (int i = x; i < x + cos(q*(pi/180)) * tmp_rand; i++) {
+			for (int j = z; j < z + sin(q*(pi/180)) * tmp_rand; j++) {
+				locateWater(createWater(), i, y, j);
+			}
+		}
+	}
+	
 }
 
 void park(int x, int y, int z) {
